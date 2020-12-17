47e85321f52c7e703e7be9e35cb3b66e5926d7c7
Choi Suman <voltovo87@gmail.com>
2020-12-17 23:43:39 +0900

    feat: toDoList delete

--- todo.js
+++ todo.js
@@ -3,16 +3,33 @@ const toDoFrom = document.querySelector(".js-toDoForm"),
     toDoList = document.querySelector(".toDoList");
 
 const TODOS_LS = "toDos";
-const toDos = [];
+let toDos = [];
+
+function filterFn(toDo){
+    return toDo.id === 1;
+};
+
+function deleteToDo(event){
+
+    const btn = event.target;
+    const li = btn.parentNode;
+    toDoList.removeChild(li);
+    const cleanToDos = toDos.filter(function (toDo){
+        return toDo.id !== parseInt(li.id);
+    });
+    toDos = cleanToDos;
+    saveToDos();
+};
 
 function saveToDos(){
     localStorage.setItem(TODOS_LS, JSON.stringify(toDos));
-}
+};
 
 function paintToDo(text){
     const li = document.createElement("li");
     const delBtn = document.createElement("button");
     delBtn.innerText = "X";
+    delBtn.addEventListener("click", deleteToDo);
     const span = document.createElement("span");
     const newId = toDos.length + 1;
     span.innerHTML = text;
@@ -23,10 +40,10 @@ function paintToDo(text){
     const toDosObj = {
         text : text,
         id : newId
-    }
+    };
     toDos.push(toDosObj);
     saveToDos();
-}
+};
 
 function handleSubmit(event){
     //form submit시 자동으로 새로고침이 되는데 그것을 막는다
@@ -35,7 +52,7 @@ function handleSubmit(event){
     const currentValue = toDoInput.value;
     paintToDo(currentValue);
     toDoInput.value = "";
-}
+};
 
 function loadToDos(){
     const loadedToDos = localStorage.getItem(TODOS_LS);
@@ -45,12 +62,12 @@ function loadToDos(){
         parsedToDos.forEach(function(toDo){
             paintToDo(toDo.text);
         });
-    }
-}
+    };
+};
 
 function init(){
     loadToDos();
     toDoFrom.addEventListener("submit", handleSubmit);
-}
+};
 
 init();
