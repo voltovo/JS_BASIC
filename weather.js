d9e7e35fcd0973cf4439d1d5341efa273656eb03
Choi Suman <voltovo87@gmail.com>
2020-12-22 12:51:00 +0900

    feat: weather api ready and latitude, longitude add

--- weather.js
+++ weather.js
@@ -1,7 +1,18 @@
 const COORDS = 'coords';
+const API_KEY = "eec5e330f5ade4183c386d6a8b0f65d2";
+
+function saveCoords(coordsObj){
+    localStorage.setItem(COORDS, JSON.stringify(coordsObj));
+}
 
 function handleGeoSucces(position){
-    console.log(position);
+    const latitude = position.coords.latitude;
+    const longitude = position.coords.longitude;
+    const coordsObj = {
+        latitude,
+        longitude
+    };
+    saveCoords(coordsObj);
 }
 
 function handleGeoError(){
