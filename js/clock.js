e858c6bca2d68bd574877ac05e3a2c4dea18862d
voltovo87 <voltovo87@gmail.com>
2020-12-26 12:15:19 +0900

    fix: clock modify

--- js/clock.js
+++ js/clock.js
@@ -1,20 +1,22 @@
-const clockContainer = document.querySelector(".js-clock"),
-      clockTitle = clockContainer.querySelector("h1");
+const clock = document.querySelector(".js-clock .clock_text");
 
 function getTime(){
     const date = new Date();
     const hours = date.getHours();
     const minutes = date.getMinutes();
     const seconds = date.getSeconds();
-    clockTitle.innerHTML = 
+    clock.innerHTML = 
     `${hours > 9 ? hours : `0${hours}`}
     :${minutes > 9 ? minutes : `0${minutes}`}
     :${seconds > 9 ? seconds : `0${seconds}`}`;
+
+    return;
 }
 
 function init(){
     getTime();
     setInterval(getTime, 1000);
+    return;
 }
 
 init();
