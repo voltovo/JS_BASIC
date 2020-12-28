1d00acdf0d76caa2beb4fb029c1f3cfffe8f5c9e
voltovo87 <voltovo87@gmail.com>
2020-12-28 13:14:29 +0900

    fix: greetings modify

--- js/greeting.js
+++ js/greeting.js
@@ -1,6 +1,7 @@
 const form = document.querySelector(".js-form"),
     input = form.querySelector("input"),
-    greeting = document.querySelector(".js-greetings");
+    greeting = document.querySelector(".js-greetings"),
+    nameContainer = document.querySelector(".js-name");
 
 const USER_LS = "currentUser",
     SHOWING_CN = "showing";
@@ -29,6 +30,7 @@ function paintGreeting(text){
     form.classList.remove(SHOWING_CN);
     greeting.classList.add(SHOWING_CN);
     greeting.innerHTML = `Hello ${text}`;
+    nameContainer.appendChild(greeting);
 };
 
 function loadName(){
